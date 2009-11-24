3292a20c4e5b6d2aa8de3d1accdb1905cdd05ead
Alex Barth <ab@a.local>
2009-11-24 17:58:02 -0500

    Tune login form.

--- profiles/osso_relying/modules/openid_sso/openid_sso.js
+++ profiles/osso_relying/modules/openid_sso/openid_sso.js
@@ -4,5 +4,13 @@
  */
 
 Drupal.behaviors.openid_sso = function() {
+
+  // Submit auto-submit forms.
   $('.auto-submit').submit();
+
+  // Hide submit button and submit form, hide noscript content, show script
+  // content and submit.
+  $('.auto-submit input:submit').hide();
+  $('.auto-submit .noscript').hide();
+  $('.auto-submit .script').show();
 }
