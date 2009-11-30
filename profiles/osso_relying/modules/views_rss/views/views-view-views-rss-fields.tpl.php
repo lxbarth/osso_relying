281c76e644577b151ae733e447b87b4abb085c2c
Alex Barth <ab@a.local>
2009-11-30 17:58:30 -0500

    Add DC creator support.

--- profiles/osso_relying/modules/views_rss/views/views-view-views-rss-fields.tpl.php
+++ profiles/osso_relying/modules/views_rss/views/views-view-views-rss-fields.tpl.php
@@ -1,7 +1,7 @@
 <?php // $Id: views-view-views-rss-fields.tpl.php,v 1.1 2009/09/22 16:49:55 rsoden Exp $ ?>
 <?php print "<?xml"; ?> version="1.0" encoding="utf-8" <?php print "?>"; ?>
 
-<rss version="2.0"<?php if($georss){ print ' xmlns:georss="http://www.georss.org/georss"'; }?>>
+<rss version="2.0" <?php print $namespaces ?>>
   <channel>
     <title><?php print $viewtitle; ?></title>
     <description><?php print $description; ?></description>
